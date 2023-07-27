3da0e5ed2be5e550e82673572e84f1254c65afdf
marco <marcogomesr@gmail.com>
2023-07-27 09:37:52 +0200

    add header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,10 @@ export default function RootLayout ({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className='container mx-auto py-8'>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header () {
+  return (
+    <h1><Link href='/'>Podcaster</Link></h1>
+  )
+}
