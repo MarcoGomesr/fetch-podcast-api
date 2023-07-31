986c4746d0a6a01882f74113709bc7e235ad7244
marco <marcogomesr@gmail.com>
2023-07-31 14:18:13 +0200

    add style on feature image

--- src/components/podcastList.tsx
+++ src/components/podcastList.tsx
@@ -6,15 +6,21 @@ import Link from 'next/link'
 interface Props {
   items: PodcastProps
 }
-
 function podcastList ({ items }: Props) {
   return (
-    <Link href={`/podcast/${items.id}`}>
-      <Image width={150} height={150} src={items.image} alt='Podcast Cover' />
-      <h2>{items.title}</h2>
-      <p>Author: <span>{items.author}</span></p>
+    <Link href={`/podcast/${items.id}`} className='relative'>
+      <section className='border border-1 border-gray-300 shadow-md p-5 mb-5 transform transition-all hover:-translate-y-2 hover:shadow-xl'>
+        <div className='flex flex-col items-center relative'>
+          <div className='w-100 h-100 overflow-hidden -mt-16 '>
+            <Image width={100} height={100} src={items.image} alt='Podcast Cover' className='border rounded-full' />
+          </div>
+          <h2 className='text-xl text-center text-gray-900 uppercase'>{items.title}</h2>
+          <p className='mt-1 text-sm text-gray-700 whitespace-no-wrap overflow-hidden max-w-full'>
+            Author: <span>{items.author}</span>
+          </p>
+        </div>
+      </section>
     </Link>
   )
 }
-
 export default podcastList
