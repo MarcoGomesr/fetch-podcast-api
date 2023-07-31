db2eaf0a70ab621c82bb338f73725598fcce70b9
marco <marcogomesr@gmail.com>
2023-07-31 14:18:34 +0200

    make loadingbounce component

---
+++ src/components/LoadingBounce.tsx
@@ -0,0 +1,9 @@
+export const LoadingBounce = () => {
+  return (
+    <div className='relative'>
+      <div className='absolute -top-14 right-0'>
+        <div className='w-4 h-4 bg-blue-600 rounded-full animate-bounce ' />
+      </div>
+    </div>
+  )
+}
