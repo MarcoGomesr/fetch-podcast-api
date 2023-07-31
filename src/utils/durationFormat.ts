6b4acf42acfb8a1bdfeddf1c0e32159db534dcf3
marco <marcogomesr@gmail.com>
2023-07-31 16:04:28 +0200

    fix duration time

--- src/utils/durationFormat.ts
+++ src/utils/durationFormat.ts
@@ -1,7 +1,10 @@
 export function getDurationFormat (duration: string) {
   if (duration === undefined) return 'No avaiable'
   let seconds = Math.floor(Number(duration) / 1000)
+  const hours = Math.floor(seconds / 3600)
+  seconds %= 3600
   const minutes = Math.floor(seconds / 60)
-  seconds = seconds % 60
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  seconds %= 60
+
+  return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
